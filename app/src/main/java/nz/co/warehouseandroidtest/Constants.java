903deb2caafff444abc402904b80da9914ce1acf
Jaemoon Hwang <jaemoon.hwang@deltapath.com>
2021-06-12 10:54:15 +1200

    add subscriptionKey field in gradle in order to hide a private Subscription-Key from code - need to put the Subscription-Key in local.properties for all api calls

--- app/src/main/java/nz/co/warehouseandroidtest/Constants.java
+++ app/src/main/java/nz/co/warehouseandroidtest/Constants.java
@@ -4,6 +4,10 @@ public class Constants {
     public static final String HTTP_URL_ENDPOINT = "https://twg.azure-api.net/";
     public static final String PREF_USER_ID = "userId";
     public static final int BRANCH_ID = 208;
-    public static final String SUBSCRIPTION_KEY = "";   // Please fill in the SUBSCRIPTION_KEY given to you here.
+    /**
+     * Please fill in subscriptionKey, the local.properties given to you here.
+     * e.g. subscriptionKey="79a8b12dca4567891a00fad111a36a44f"
+     */
+    public static final String SUBSCRIPTION_KEY = BuildConfig.SUBSCRIPTION_KEY;
     public static final String MACHINE_ID = "1234567890";
 }
